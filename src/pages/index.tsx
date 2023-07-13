836ed1b7afe1e51b1ba6b98cf8179357217ea0c9
Surya Ashish <ashishsurya126@gmail.com>
2023-07-13 13:02:52 +0530

    external accounts ui added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,10 @@ import {
 	SignOutButton,
 	SignedIn,
 	SignedOut,
+	UserButton,
 	useUser,
 } from "@clerk/nextjs";
+import { ExternalAccountResource } from "@clerk/types";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,16 +30,20 @@ export default function Home() {
 		<div
 			className={twMerge(inter.className, " grid place-items-center h-screen")}
 		>
-			<div className="text-center space-y-3">
+			<div className=" space-y-3">
 				<SignedIn>
-					<h1 className="text-7xl">You are signed in!, can stay here</h1>
+					<h1 className="text-6xl">Welcome, {user?.fullName}</h1>
 					<SignOutButton />
 
 					<h3 className="text-4xl">Your account linked to</h3>
-					{/* <pre>{JSON.stringify(user, null, 2)}</pre> */}
-					{user?.primaryEmailAddress?.linkedTo.map((provider) => (
-						<p className="capitalize">{provider.type.split("_")[1]}</p>
-					))}
+					{/* <pre className="text-[8px]">{JSON.stringify(user, null, 2)}</pre> */}
+					{/* {user?.primaryEmailAddress?.linkedTo.map((provider) => (
+						<p key={provider.id} className="capitalize">
+							{provider.type.split("_")[1]}
+						</p>
+					))} */}
+
+					<ConnectedAccounts accounts={user?.externalAccounts} />
 				</SignedIn>
 
 				<SignedOut>
@@ -48,3 +54,33 @@ export default function Home() {
 		</div>
 	);
 }
+
+interface ConnectedAccountProps {
+	accounts: ExternalAccountResource[] | undefined;
+}
+
+const ConnectedAccounts: React.FC<ConnectedAccountProps> = ({ accounts }) => {
+	if (accounts === undefined) {
+		return (
+			<p className="text-xl text-red-600">
+				Account not linked to Twitter, LinkedIn etc.....
+			</p>
+		);
+	}
+	return (
+		<div className=" divide-y-2">
+			{/* <pre>{JSON.stringify(accounts, null, 2)}</pre> */}
+			{accounts.map((account) => (
+				<div className="flex items-center space-x-3 py-4">
+					<img
+						src={account.avatarUrl}
+						loading="lazy"
+						alt=""
+						className="w-10 h-10 rounded-full"
+					/>
+					<p className="capitalize font-semibold">{account.provider}</p>
+				</div>
+			))}
+		</div>
+	);
+};
